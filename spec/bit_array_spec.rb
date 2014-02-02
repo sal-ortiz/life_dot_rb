d14a53b9501157f87633633b524ed06777e768b1
Sal <this.is.salvador.ortiz@gmail.com>
2014-02-01 21:51:02 -0600

    moved BirArrayHelper tests to their own spec

--- spec/bit_array_spec.rb
+++ spec/bit_array_spec.rb
@@ -3,55 +3,6 @@ require File.join( File.dirname( __FILE__ ), '.', 'lib', 'spec_helper.rb' )     
 require File.join( File.dirname( __FILE__ ), '..', 'lib', 'bit_array.rb' )        # our source code to be tested.
 
 
-describe "the BitArrayHelper module" do
-
-  it "exists and is a module" do
-    expect {
-      expect( BitArrayHelper.class ).to be( Module )
-    }.not_to raise_error
-  end # it "exists and is a module"
-
-
-  describe "the BitArrayHelper::pack_data method" do
-
-    it "should pack a given array into a integer value" do
-      test_data = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]                # expected input
-      expected_data = 0b1001100001110110010101000011001000010000  # expected output
-
-      expect( BitArrayHelper::pack_array( test_data, 4 ) ).to eq( expected_data )
-    end # it "should pack a given array into a integer value"
-
-  end # describe "the BitArrayHelper::pack_data method"
-
-
-  describe "the BitArrayHelper::unpack_data method" do
-
-    it "should convert given packed data, in the form of an integer, into an Array object" do
-      test_data = 0b1001100001110110010101000011001000010000    # expected input
-      expected_data = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]          # expected output
-
-      expect( BitArrayHelper::unpack_array( test_data, expected_data.length, 4 ) ).to match_array( expected_data )
-    end # it "should convert given packed data, in the form of an integer, into an Array object"
-
-  end # describe "the BitArrayHelper::unpack_data method"
-
-
-  describe "the BitArrayHelper::num_bits method" do
-
-    it "should correctly indicate the minimum number of bits required for a given integer" do
-      max_num_bits = 1024   # test accuracy up to this number of bits.
-
-      max_num_bits.times do |bit_width|
-        expect( BitArrayHelper::num_bits( (2**bit_width)-1 ) ).to eq( bit_width )
-      end
-
-    end # it "should correctly indicate the minimum number of bits required for a given integer"
-
-  end # describe "the BitArrayHelper::num_bits method"
-
-end # describe "the BitArrayHelper module"
-
-
 describe "a BitArray object" do
   it "should initialize an empty array object if no arguments are given" do
     the_object = BitArray.new
